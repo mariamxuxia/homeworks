4dd45aea4ad19044ec269cd1440f74a430148663
mariamxuxia <mariamxuxia0@gmail.com>
2025-03-14 22:20:58 +0400

    commit

---
+++ day10/homework/hw4.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter your number"))
+num2 = int(input("enter your second number"))
+
+if num1 > num2:
+   print("num1 is more then num2")
+elif num1 < num2:
+    print("num1 is less then num2")
+else:
+    print("num1 is equal to num2")
---
+++ day10/homework/hw5.py
@@ -0,0 +1,8 @@
+num1 = int(input("please enter your number: "))
+
+if num1 > 10:
+   print("num1 doesn't equal to 10")
+elif num1 < 10:
+    print("num1 doesn't equal to 10")
+else:
+    print("equals to 10")
