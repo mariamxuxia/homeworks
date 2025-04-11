f08e340a40fa3c473b7ea0bcc4bea4cbd068e9ae
mariamxuxia <mariamxuxia0@gmail.com>
2025-04-11 20:45:26 +0400

    commit

---
+++ day18/homework/hw1.py
@@ -0,0 +1,3 @@
+#for loop ის მეშვეობით გამოიტანე GOA BEST 1000 ჯერ
+for i in range(1001):
+    print(i,"gos best")
---
+++ day18/homework/hw3.py
@@ -0,0 +1,4 @@
+#მომხმარებელს შემოატანინე რაიმე რიცხვი, შემდეგ for loop ში შემოტანილი რიცხვის მეშვეობით უნდა გამოიტანოს GOA Best იმდენჯერ რასაც მომხმარებელი შეიყვანს
+Number = int(input("Enter Number: "))
+for i in range(0,Number,1):
+    print(str(i) + " GOA BEST")
